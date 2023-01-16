6b483285b7cced632025f2660d6178057bd1cf3b
palumbolorenzo <palumbolorenzo97@gmail.com>
2023-01-16 10:25:41 +0100

    provvisorio

---
+++ config/menu.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    [
+        'route' => 'home',
+        'label' => 'Home',
+    ],
+    [
+        'route' => 'characters',
+        'label' => 'Characters',
+    ],
+    [
+        'route' => 'comics',
+        'label' => 'Comics',
+    ],
+    [
+        'route' => 'movies',
+        'label' => 'Movies',
+    ],
+    [
+        'route' => 'tv',
+        'label' => 'Tv',
+    ],
+    [
+        'route' => 'games',
+        'label' => 'Games',
+    ],
+    [
+        'route' => 'collectibles',
+        'label' => 'Collectibles',
+    ],
+    [
+        'route' => 'videos',
+        'label' => 'Videos',
+    ],
+    [
+        'route' => 'fans',
+        'label' => 'Fans',
+    ],
+    [
+        'route' => 'news',
+        'label' => 'News',
+    ],
+    [
+        'route' => 'shop',
+        'label' => 'Shop',
+    ],
+];
