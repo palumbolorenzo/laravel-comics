ceb63228e9d91711e535c0baf2e0c7e3ecc994fd
palumbolorenzo <palumbolorenzo97@gmail.com>
2023-01-17 17:02:36 +0100

    provvisorio

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,12 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
+    @include('partials.header')
 
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
 </body>
 </html>
